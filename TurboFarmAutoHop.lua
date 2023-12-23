9468a95482cd1e09731e91fc243f49838c8b015c
xXxTURBOxX <104566671+xXxTURBOxX@users.noreply.github.com>
2023-12-23 14:16:32 +0200

    Update TurboFarmAutoHop.lua

--- TurboFarmAutoHop.lua
+++ TurboFarmAutoHop.lua
@@ -30,7 +30,7 @@ local function TPReturner(placeId)
 	for i,v in pairs(Site.data) do
 		local Possible = true
 		ID = tostring(v.id)
-		if tonumber(v.maxPlayers) - tonumber(v.playing) <=2 then
+		if tonumber(v.maxPlayers) - tonumber(v.playing) <=3 then
 			for _,Existing in pairs(AllIDs) do
 				if num ~= 0 then
 					if ID == tostring(Existing) then
